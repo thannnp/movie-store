b01735899948902eb6718b6abfd97866ef603357
Back <tananan.p@topcloud.co>
2026-05-16 05:17:25 +0700

    feat(auth): add login page and form component

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/login/login-form"
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-muted">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
